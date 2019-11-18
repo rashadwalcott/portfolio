32040338b4701e0eda09ff4d2e71b8b22846326f
Rashad <rashad.walcott15@gmail.com>
2019-11-18 15:50:07 -0500

    Completes Landing Page

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import {Tabs, Tab, Grid, Cell, Card, CardTitle, CardActions,Button,CardMenu,IconButton} from 'react-mdl';
 class Projects extends React.Component {
-  render (){
 
+  state = {
+    activeTab: 0
+  }
+
+  toggleCategories(){
+
+    if(this.state.activeTab === 0){
+      return (
+        <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+          <CardTitle style={{color: '#fff', height: '176px', background: 'url() center / cover'}}></CardTitle>
+
+        </Card>
+      )
+    }
+    else if(this.state.activeTab === 1){
+      return (
+        <div><h1>This is Javascript</h1></div>
+      )
+    }
+  }
+  render (){
   return (
-    <div className = "project">
-      <h1>Project</h1>
+    <div className = "category-tabs">
+    <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
+      <Tab>React</Tab>
+      <Tab>Javascript</Tab>
+    </Tabs>
+
+    <section className='projects-grid'>
+      <Grid className='projects-grid'>
+        <Cell col={12}>
+          <div className='content'> {this.toggleCategories()}</div>
+        </Cell>
+      </Grid>
+    </section>
     </div>
   );
 }
