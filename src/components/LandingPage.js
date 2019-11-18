61be6046d6e1662f0f933abc7bd50840d9cff346
Rashad <rashad.walcott15@gmail.com>
2019-11-18 14:46:08 -0500

    Add links to icons

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -5,25 +5,40 @@ class LandingPage extends React.Component {
   render (){
 
   return (
+
     <div style ={{width:'100', margin: 'auto'}}>
       <Grid className ='landing-grid'>
         <Cell col={12}>
           <img src ={mypic}
             alt='avatar'
             className='avatar-img' />
+            </Cell>
+
           <div className='banner-text'>
             <h1> Full Stack Web Developer</h1>
 
           <hr/>
 
         <p>Javascript | React | Ruby | Rails | SQL | PostgreSQL | HTML | CSS | SASS</p>
+
         <div className='social-links'>
+          {/* LinkedIn*/}
           <a href='https://www.linkedin.com/in/rashadwalcott/' rel='noopener noreferrer' target="_blank">
-            <i className="fa fa-linkedin-square" aria_hidden='true'/>
+            <i className="fab fa-linkedin" aria-hidden='true'/>
           </a>
+
+          {/* Github*/}
+          <a href='https://github.com/rashadwalcott' rel='noopener noreferrer' target="_blank">
+            <i className="fab fa-github" aria-hidden='true'/>
+          </a>
+
+          {/* Medium*/}
+          <a href='https://medium.com/rashads-tech-blogs' rel='noopener noreferrer' target="_blank">
+            <i className="fab fa-medium-m" aria-hidden='true'/>
+          </a>
+          
         </div>
           </div>
-        </Cell>
       </Grid>
     </div>
   );
