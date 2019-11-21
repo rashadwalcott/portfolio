36dfc0fd1ba8f0b6520e193549125af88f2e1621
Rashad <rashad.walcott15@gmail.com>
2019-11-21 16:07:49 -0500

    Finished the sent component

--- src/components/Sent.js
+++ src/components/Sent.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import { Grid } from 'react-mdl'
 class Sent extends React.Component {
   render (){
     return (
-      <div className='sent'>
-      Thank You For Reaching Out.
-      Your Message Has Been Sent.
-      I Will Get Back To You Shortly.
+      <div className='sent-body'>
+        <Grid className='sent-grid'>
+      <p>  Your Message Has Been Sent.<br /><br />
+        Thank You For Reaching Out. <br /><br />
+      I Will Get Back To You Shortly.<br /><br /></p>
+  </Grid>
       </div>
     )
   }
