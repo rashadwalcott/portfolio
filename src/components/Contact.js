137b41ab721c3df7ef1082b23ddedf9ac490cbfd
Rashad <rashad.walcott15@gmail.com>
2019-11-21 10:49:01 -0500

    Started form for contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,42 @@ import React from 'react';
 import { Cell , Grid, Button, Textfield} from 'react-mdl'
 
 class Contact extends React.Component {
-  render (){
+  state={
+    name: '',
+    email: '',
+    message:' '
+  }
 
+  handleChange = (event)=> {
+    this.setState({[event.target.name]:event.target.value})
+  }
+  render (){
   return (
     <div className='contact-body'>
       <Grid className='contact-grid'>
         <Cell col={6}>
           <h2>Contact Me </h2>
           <hr />
+          <div>
+            <form>
+              <Textfield
+                onChange={(event)=> {this.handleChange(event)}}
+                label='Name'
+                name='name'
+                required='true'
+                style={{width: '200px'}}
+                />
+              <br/>
+                <Textfield
+                  onChange={(event)=> {this.handleChange(event)}}
+                  label='Email'
+                  name='email'
+                  required='true'
+                  style={{width: '200px'}}
+                  />
+            </form>
+          </div>
 
-        
         </Cell>
       </Grid>
     </div>
