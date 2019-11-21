d4a3e2bc9cf6664c0846a40eaddf3a195d88f89e
Rashad <rashad.walcott15@gmail.com>
2019-11-21 15:06:45 -0500

    Started on about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
+import { Cell , Grid } from 'react-mdl'
+import aboutmepic from '../aboutmepic.jpeg'
 
 class About extends React.Component {
   render (){
 
   return (
     <div>
-      <h1> About Component</h1>
+      <Grid className='about-grid'>
+        <Cell col={6}>
+          <h2>Rashad Walcott</h2>
+          <img
+            src={aboutmepic}
+            alt='avatar'
+            style={{height: '325px', fontFamily: 'American Typewriter'}} />
+          
+        </Cell>
+      </Grid>
     </div>
   );
 }
