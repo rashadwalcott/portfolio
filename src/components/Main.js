6d5ae4bce9e1a09a77de4e8b077fa626f6a25e1b
Rashad <rashad.walcott15@gmail.com>
2019-11-22 16:40:42 -0500

    Testing transitions on components

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import React, {useContext} from 'react';
+import { Switch, Route, __RouterContext} from 'react-router-dom';
+import { useTransition, animated} from 'react-spring';
 import Projects from './Projects.js';
 import Home from './LandingPage.js';
 import About from './About.js';
@@ -9,6 +10,12 @@ import Sent from './Sent.js';
 
 class Main extends React.Component {
   render (){
+    const { location} = useContext(__RouterContext);
+    const transitions = useTransition(location,location => location.pathname, {
+      from: {opacity: 0, transform: "translate(100%,0)"},
+      enter:{opacity: 1,transform: "translate(0%,0)"},
+      leave: {opacity: 0,transform: "translate(-50%,0)"}
+    });
 
   return (
     <div>
