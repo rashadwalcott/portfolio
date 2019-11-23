e51217f6cd300dc2211fbda2cb66f60c5d67a611
Rashad <rashad.walcott15@gmail.com>
2019-11-22 19:50:49 -0500

    Updates

---
+++ precache-manifest.1faadcce4a8d35976a2b795962231925.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ccf0ffac3fbac14e3afa7a9581ec23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "500f342847784d34de98",
+    "url": "/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "c2161680d77227dc042d",
+    "url": "/static/css/main.5f085b06.chunk.css"
+  },
+  {
+    "revision": "500f342847784d34de98",
+    "url": "/static/js/2.19c4bad0.chunk.js"
+  },
+  {
+    "revision": "c2161680d77227dc042d",
+    "url": "/static/js/main.b16a1eea.chunk.js"
+  },
+  {
+    "revision": "0ac2b0fd5fd4af3be109",
+    "url": "/static/js/runtime-main.8896086d.js"
+  },
+  {
+    "revision": "5cc15c260f22a9ffc7b351d92434c45d",
+    "url": "/static/media/RashadWalcott_Resume.5cc15c26.pdf"
+  },
+  {
+    "revision": "febaba32d6b9ce7fd95d96e0cc9167aa",
+    "url": "/static/media/aboutmepic.febaba32.jpeg"
+  },
+  {
+    "revision": "174c4492172a93f50ff7578ef3c6ee58",
+    "url": "/static/media/picofme.174c4492.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.752b57d37da5b4cfc95d4cbbf1bdb2af.js"
+  "/precache-manifest.1faadcce4a8d35976a2b795962231925.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
